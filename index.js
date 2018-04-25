4af8c6996bda46cb65bd2cd6a30ab9aa4acc2f9e
debck <debck7019@gmail.com>
2018-04-25 11:47:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+var url ="https://www.reddit.com/top/";
+
+request(url, function(err,response,html){
+     if(!err){
+         var $ = cheerio.load(html);
+       
+        var alldata= $("#siteTable").children();
+        
+        var data =[];
+        alldata.each(function(index){
+        
+        var result=$("#siteTable").children().eq(index).children().eq(4).find("a.title").text();
+        if(result !== "")
+            data.push(result);
+        
+        });
+        
+        console.log(data);
+     }
+});
